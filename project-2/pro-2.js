aa37c36813c98bd2076b30f3aa19af91769c1764
Aaditya Sahu <aaditya.sahu@nytelabs.co>
2026-06-18 18:23:59 +0530

    peoject-2

---
+++ project-2/pro-2.js
@@ -0,0 +1,47 @@
+let num1 = +(prompt('Enter any number.', '1'));
+let sign = (prompt('Enter the operation.', '+'));
+let num2 = +(prompt('Enter any number.', '2'));
+
+
+
+function calculate() {
+    if (sign === "+") {
+        add = num1 + num2;
+        alert(`Result is ${num1} + ${num2} = ${add} `);
+    }
+
+    if (sign === "-") {
+        let sub = num1 - num2;
+        alert(`Result is ${num1} - ${num2} = ${sub} `);
+    }
+
+    if (sign === "*") {
+        let multiply = num1 * num2;
+        alert(`Result is ${num1} * ${num2} = ${multiply} `);
+    }
+
+    if (sign === "/") {
+        let divide = num1 / num2;
+        alert(`Result is ${num1} / ${num2} = ${divide} `);
+    }
+
+}
+
+// //function add(){
+//  //  let add= num1 + num2;
+
+// }
+
+// function sub(){
+//     let sub=num1-num2;
+// }
+
+// function multiply(){
+//     let multiply= num1 * num2;
+// }
+
+// function divide(){
+//     let divide=num1 / num2;
+// }
+
+calculate();
