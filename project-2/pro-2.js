6edc1a41585a8ff92cc34bcd82858aef424b0cf1
Aaditya Sahu <aaditya.sahu@nytelabs.co>
2026-06-19 18:06:18 +0530

    fix-modulo

--- project-2/pro-2.js
+++ project-2/pro-2.js
@@ -1,51 +1,56 @@
 function calculate() {
-  let num1 = +prompt("Enter any number.", "1");
-  let sign = prompt("Enter the operation.", "+");
-  let num2 = +prompt("Enter any number.", "2");
+    let num1 = +prompt("Enter any number.", "1");
+    let sign = prompt("Enter the operation.", "+");
+    let num2 = +prompt("Enter any number.", "2");
 
-  let result = null;
+    let result = null;
 
-  switch (sign) {
-    case "+": {
-      result = num1 + num2;
-      break;
+    switch (sign) {
+        case "+": {
+            result = num1 + num2;
+            break;
+        }
+        case "-": {
+            result = num1 - num2;
+            break;
+        }
+        case "*": {
+            result = num1 * num2;
+            break;
+        }
+        case "/": {
+            result = num1 / num2;
+            break;
+        }
+        case "%": {
+            result = num1 % num2;
+            alert("Modulo is a mathematical operation that returns the remainder left over after one number is divided by another");
+            break;
+        }
+        default: {
+            alert("operator is wrong");
+        }
     }
-    case "-": {
-      result = num1 - num2;
-      break;
-    }
-    case "*": {
-      result = num1 * num2;
-      break;
-    }
-    case "/": {
-      result = num1 / num2;
-      break;
-    }
-    default: {
-      alert("operator is wrong");
-    }
-  }
 
-  if (result === null) {
-    return;
-  }
+    if (result === null) {
+        return;
+    }
 
-  alert(`Result is ${num1} ${sign} ${num2} = ${result}`);
+    alert(`Result is ${num1} ${sign} ${num2} = ${result}`);
 
-  claculateAgain();
+    claculateAgain();
 }
 
 function claculateAgain() {
-  const again = confirm("Want to calculate again?");
+    const again = confirm("Want to calculate again?");
 
-  if (!again) {
-    alert("Thank You for using calculator.");
+    if (!again) {
+        alert("Thank You for using calculator.");
 
-    return;
-  }
+        return;
+    }
 
-  calculate();
+    calculate();
 }
 
 calculate();
